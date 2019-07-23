452ff38735e31a2657f32c9cc0b5c6e2ef00824f
andbacha <andrzej.bachanowicz@gmail.com>
2019-07-23 17:52:18 +0200

    Executable created using Launch4j

---
+++ src/main/java/com/gui/MainMenu.java
@@ -0,0 +1,113 @@
+package com.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.util.HashMap;
+
+public class MainMenu {
+
+    @FXML
+    private MenuItem menuNewSeason;
+
+    @FXML
+    private MenuItem menuLoadSeason;
+
+    @FXML
+    private MenuItem menuSaveSeason;
+
+    @FXML
+    private MenuItem menuExportPlayers;
+
+    @FXML
+    private MenuItem menuImportPlayers;
+
+    @FXML
+    private MenuItem menuClose;
+
+    @FXML
+    private MenuItem menuAddTournament;
+
+    @FXML
+    private MenuItem menuAddPlayer;
+
+    @FXML
+    private MenuItem menuModifyTournament;
+
+    @FXML
+    private MenuItem menuModifyPlayer;
+
+    @FXML
+    private MenuItem menuRemoveTournament;
+
+    @FXML
+    private MenuItem menuRemovePlayer;
+
+    @FXML
+    private MenuItem menuAbout;
+
+    @FXML
+    private Button buttonNewSeason;
+
+    @FXML
+    private Button buttonNewTournament;
+
+    @FXML
+    private Button buttonRemoveTournament;
+
+    @FXML
+    private Button buttonImportPlayers;
+
+    @FXML
+    private Button buttonExportPlayers;
+
+    @FXML
+    private Label labelBottom;
+
+    @FXML
+    private Hyperlink hyperlinkMatches;
+
+    @FXML
+    private Hyperlink hyperlinkTournamentTable;
+
+    @FXML
+    private Hyperlink hyperlinkPlayers;
+
+    @FXML
+    private Hyperlink hyperlinkSeasonTable;
+
+    @FXML
+    private Hyperlink hyperlinkTournamentRules;
+
+    @FXML
+    private Hyperlink hyperlinkSeasonRules;
+
+    @FXML
+    private Hyperlink hyperlinkEndTournament;
+
+    @FXML
+    private Hyperlink hyperlinkEndSeason;
+
+    @FXML
+    private AnchorPane anchorPaneCenterContent;
+
+    public void setCenterContent(ActionEvent actionEvent) throws Exception {
+        Hyperlink hyperlink = (Hyperlink) actionEvent.getSource();
+        HashMap<String, String> hyperlinks = Hyperlinks.getHyperlinks();
+        Pane loadedPane = FXMLLoader.load(getClass().getResource(hyperlinks.get(hyperlink.getId())));
+        anchorPaneCenterContent.getChildren().clear();
+        anchorPaneCenterContent.getChildren().add(loadedPane);
+        AnchorPane.setTopAnchor(loadedPane, 0.0);
+        AnchorPane.setBottomAnchor(loadedPane, 0.0);
+        AnchorPane.setLeftAnchor(loadedPane, 0.0);
+        AnchorPane.setRightAnchor(loadedPane, 0.0);
+    }
+
+}
